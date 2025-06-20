329a14aa5a0c0d14e2cb3324d685a1308f4e9d3d
Selina Cheng <selinajcheng@gmail.com>
2025-06-20 15:09:50 -0400

    Remove cloned repo COMFORT (from Zhang et al. 2025) from tracking (sorry, still new to Git)

--- dualrep/COMFORT/models/GLAMM/glamm/__init__.py
+++
@@ -1,4 +0,0 @@
-from .GLaMM import GLaMMForCausalLM
-from .llava import conversation as conversation_lib
-from .SAM.utils.transforms import ResizeLongestSide
-from .llava.mm_utils import tokenizer_image_token
